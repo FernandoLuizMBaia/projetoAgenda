74ecb11e2d547276e36f902b3883802c57bc0deb
Fernando <cinereolupus705@gmail.com>
2024-05-02 19:47:42 -0300

    varias coisas

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,19 +1,20 @@
 exports.middlewareGlobal = (req, res, next) => {
-    res.locals.umaVariavelLocal = 'Este é o valor da variavel local.';
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
     next();
 };
 
-exports.outroMiddlewareGlobal = (req, res, next) => {    
+exports.outroMiddlewareGlobal = (req, res, next) => {
     next();
 };
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err){
+    if (err) {
         return res.render('404');
     }
 };
 
-exports.csrfMiddleware = (req, res, next) =>{
+exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken();
     next();
 };
