7068c665d95c13c7faab758c44d1210cdbdf13e9
Fernando <cinereolupus705@gmail.com>
2024-05-02 19:47:18 -0300

    enumerables mudanças

--- routes.js
+++ routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
-const loginController= require('./src/controllers/loginController');
+const loginController = require('./src/controllers/loginController');
 
 // Rotas da home
 route.get('/', homeController.index);
 
 // Rotas de loguin
-route.get('/login/index', loginController.index)
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
 
 module.exports = route;
