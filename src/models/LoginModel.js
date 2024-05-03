9fbf31681444c82f3e28e4706415e835bd535253
Fernando <cinereolupus705@gmail.com>
2024-05-03 12:22:58 -0300

    já sabe, né? mais mudaças

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
@@ -11,32 +12,55 @@ const LoginModel = mongoose.model('Login', LoginSchema);
 class Login {
     constructor(body) {
         this.body = body;
-        this.errors = []
-        this.user = null
+        this.errors = [];
+        this.user = null;
+    }
+
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if (!this.user) {
+            this.errors.push('Usuario não existe');
+            return;
+        }
+
+        if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha Invalida');
+            this.user = null;
+            return;
+        }
     }
 
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
-        }
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
+        this.user = await LoginModel.create(this.body);
+    }
+
+    async userExists() {
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Tu já tem conta, irmão.');
     }
 
     valida() {
         this.cleanUp();
         if (!validator.isEmail(this.body.email)) this.errors.push('Seu e-mail não presta irmão');
-        if (this.body.password.length < 3 || this.body.password.length > 22) {
-            this.errors.push('A senha deve ter entre 3 a 22');
-        }
+        if (this.body.password.length < 3 || this.body.password.length > 22) this.errors.push('A senha deve ter entre 3 a 22');
     }
 
     cleanUp() {
         for (const key in this.body) {
-            if (typeof this.body[key] !== 'string') {
+            if (!Array.isArray(this.body[key]) && typeof this.body[key] !== 'string') {
                 this.body[key] = '';
             }
         }
